1aa304f4b2d8b873e92dd892a619a6313cb18e1d
sanuv9683 <sanuv9683@gmail.com>
2024-11-03 00:54:55 +0000

    Content Updated

---
+++ dist/js/values.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = '936125713810-vllc3mt30976jfllsg3i40ak55lofvl1.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCsWAr78ZezVoY0xgLQj8K-K6dJuR_McSc';
+const SPREADSHEET_ID = '1Mifn_2UvP5N7cxY9p0guNeU97yDHGBTCRrdT9bXY-I8';
+const SHEET_NAME = 'Sheet1';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
